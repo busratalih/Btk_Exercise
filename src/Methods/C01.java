f254893fcfd05a3dd57bd50b8037c66893a012d4
Büşra Talih <busra.talihhh@gmail.com>
2022-11-10 15:22:43 +0300

    third commit

--- src/Methods/C01.java
+++ src/Methods/C01.java
@@ -1,2 +1,31 @@
-package Methos;public class C01 {
+package Methods;
+
+public class C01 {
+
+    public static void main(String[] args) {
+
+    sayiBulmaca();
+
+    }
+
+    public static void sayiBulmaca(){
+        int [] sayilar=new int [] {1,2,5,7,9,0};
+        int aranacak=6;
+        boolean varMi=false;
+
+        for (int sayi:sayilar){
+            if (sayi==aranacak){
+                varMi=true;
+                break;
+            }
+        }
+
+        if (varMi){
+            System.out.println("sayi mevcuttur");
+        }else {
+            System.out.println("sayi mevcut degildir");
+        }
+
+    }
+
 }
